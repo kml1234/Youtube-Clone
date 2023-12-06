ea959b60b577aa202f9c79e03a7f566a506d1354
kml1234 <108321400+kml1234@users.noreply.github.com>
2023-12-06 20:37:06 +0530

    Update contains.js

--- src/utils/contains.js
+++ src/utils/contains.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY="AIzaSyDosLkl5uuHwAF32hBOLJFmIHlPFAhNP8c";
+const GOOGLE_API_KEY=Write your api key here;
 
 export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 
